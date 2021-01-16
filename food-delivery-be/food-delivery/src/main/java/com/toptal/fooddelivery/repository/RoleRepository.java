1b7129665a2e38a16c60ec95a8298d74cf82e3ee
Huhurez Marius <huhurezmarius@yahoo.com>
2021-01-16 18:36:20 +0200

    Security implementation

---
+++ food-delivery-be/food-delivery/src/main/java/com/toptal/fooddelivery/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.toptal.fooddelivery.repository;
+
+import com.toptal.fooddelivery.model.Role;
+import com.toptal.fooddelivery.enums.RoleEnum;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleEnum name);
+}
